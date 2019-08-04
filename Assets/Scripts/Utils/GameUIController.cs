8aaa2441c92e12775339da956611ecadf4e205b7
David Maidenberg <david.maidenberg@epitech.eu>
2019-08-03 19:24:08 -0700

    Add SoundManager Script and link it with UI

---
+++ Assets/Scripts/Utils/GameUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameUIController : MonoBehaviour
+{
+    public Button soundButton;
+    public Sprite[] soundButtonImages;
+
+    public void OnVolumeChange()
+    {
+        MusicPlayer.instance.ChangeVolume();
+        soundButton.image.sprite = soundButtonImages[MusicPlayer.instance.volume];
+    }
+}
---
+++ Assets/Scripts/Utils/MusicPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer instance = null;
+
+    public AudioClip[] audioClips;
+    public int volume = 2;
+
+    private AudioSource audioSource;
+    private int audioIndex = 1;
+    private bool volumeSwitch = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = audioClips[audioIndex];
+            audioSource.Play();
+            audioIndex = (audioIndex + 1) % audioClips.Length;
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        if (!volumeSwitch)
+            volume -= 1;
+        else
+            volume += 1;
+        if (volume == 0 || volume == 2)
+            volumeSwitch = !volumeSwitch;
+        audioSource.volume = 0.4f * (float)volume;
+    }
+}
